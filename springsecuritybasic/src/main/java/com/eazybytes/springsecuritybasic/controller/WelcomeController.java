dde0b402827c0b449de0eacb9633ad0372f25103
sanchi285 <bismoy.sanchi@gmail.com>
2023-06-29 23:07:39 +0600

    Basic Spring Security added

---
+++ springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/Welcome")
+    public String sayWelcome(){
+        return "Welcome To spring application without security";
+    }
+}
